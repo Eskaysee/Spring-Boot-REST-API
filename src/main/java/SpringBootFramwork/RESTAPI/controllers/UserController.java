25a4441bcfe826ba57e35be5c73b7ca83bea8b59
Sihle Calana <CLNSIH001@myuct.ac.za>
2024-03-12 00:14:24 +0200

    Creating a Social media REST API where users make post. Created implementation of find, find by id, delete by id & save for both resources (user and post). This makes use of GET, POST & DELETE commands

---
+++ src/main/java/SpringBootFramwork/RESTAPI/controllers/UserController.java
@@ -0,0 +1,37 @@
+package SpringBootFramwork.RESTAPI.controllers;
+
+import SpringBootFramwork.RESTAPI.beans.User;
+import SpringBootFramwork.RESTAPI.services.UserDaoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private UserDaoService service;
+
+    public UserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User findById(@PathVariable int id) {
+        return service.findById(id);
+    }
+
+    @PostMapping("/users")
+    public void addUser(@RequestBody User user) {
+        service.save(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public boolean deleteById(@PathVariable int id) {
+        return service.deleteById(id);
+    }
+}
---
+++ src/main/java/SpringBootFramwork/RESTAPI/services/PostDaoService.java
@@ -0,0 +1,42 @@
+package SpringBootFramwork.RESTAPI.services;
+
+import SpringBootFramwork.RESTAPI.beans.Post;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PostDaoService {
+
+    private static List<Post> posts = new ArrayList<>();
+    private static int postCOunt = 0;
+
+    static {
+        posts.add(new Post(++postCOunt, "This is my first post"));
+        posts.add(new Post(++postCOunt, "Second Post"));
+        posts.add(new Post(++postCOunt, "This is my third post"));
+    }
+
+    public static List<Post> findAll() {
+        return posts;
+    }
+
+    public Post findById(int id) {
+        for (Post post : posts) {
+            if (post.getId() == id) {
+                return post;
+            }
+        }
+        return null;
+    }
+
+    public void savePost(Post post) {
+        post.setId(++postCOunt);
+        posts.add(post);
+    }
+
+    public  boolean deleteById(int id) {
+        return posts.removeIf(post -> post.getId() == id);
+    }
+}

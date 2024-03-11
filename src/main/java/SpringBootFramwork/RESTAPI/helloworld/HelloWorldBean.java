1cb151c8fcca9ca6fd9e46b0d978b1fb71891381
Sihle Calana <CLNSIH001@myuct.ac.za>
2024-03-11 17:34:25 +0200

    Hello World REST API now returns a Bean

---
+++ src/main/java/SpringBootFramwork/RESTAPI/helloworld/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package SpringBootFramwork.RESTAPI.helloworld;
+
+import org.springframework.context.annotation.Bean;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
--- src/main/java/SpringBootFramwork/RESTAPI/helloworld/HelloWorldController.java
+++ src/main/java/SpringBootFramwork/RESTAPI/helloworld/HelloWorldController.java
@@ -1,18 +1,18 @@
 package SpringBootFramwork.RESTAPI.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//Exposes this as a REST API
 @RestController
 public class HelloWorldController {
 
-    //Provides a URL for this method
-    //@RequestMapping(method = RequestMethod.GET, path = "/hello-world") //Equivalent to the below
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 }

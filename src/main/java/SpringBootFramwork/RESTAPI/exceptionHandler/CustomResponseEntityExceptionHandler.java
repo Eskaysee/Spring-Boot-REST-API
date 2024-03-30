5234d5d80df7869152c9e5605d1312125c886fe5
Sihle Calana <CLNSIH001@myuct.ac.za>
2024-03-30 15:15:02 +0200

    ErrorDetails determines the structure of error responses. Also created a custom exception handler to handle thrown exceptions

---
+++ src/main/java/SpringBootFramwork/RESTAPI/exceptionHandler/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,37 @@
+package SpringBootFramwork.RESTAPI.exceptionHandler;
+
+import SpringBootFramwork.RESTAPI.beans.User;
+import SpringBootFramwork.RESTAPI.exceptions.ErrorDetails;
+import SpringBootFramwork.RESTAPI.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails error = new ErrorDetails(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails error = new ErrorDetails(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}

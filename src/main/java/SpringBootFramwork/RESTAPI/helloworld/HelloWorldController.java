a429f5d1e948c124261466532c097126f8c4dbbf
Sihle Calana <CLNSIH001@myuct.ac.za>
2024-04-18 17:24:38 +0200

    made use of i18n to translate greetings based on the user's locale

--- src/main/java/SpringBootFramwork/RESTAPI/helloworld/HelloWorldController.java
+++ src/main/java/SpringBootFramwork/RESTAPI/helloworld/HelloWorldController.java
@@ -1,13 +1,24 @@
 package SpringBootFramwork.RESTAPI.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public String helloWorld() {
-        return "Hello World";
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("greeting.message", null, "Greetings", locale);
     }
 
     @GetMapping(path = "/hello-world-bean")

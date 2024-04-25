322c6bb275ed55237b7e5010319a925481b483f0
Sihle Calana <CLNSIH001@myuct.ac.za>
2024-04-25 21:35:46 +0200

    Customized the REST API response for users, particularly how some field names are presented and removing some fields from the response using static filtering.

--- src/main/java/SpringBootFramwork/RESTAPI/beans/User.java
+++ src/main/java/SpringBootFramwork/RESTAPI/beans/User.java
@@ -1,18 +1,23 @@
 package SpringBootFramwork.RESTAPI.beans;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
-import java.util.List;
 
+@JsonIgnoreProperties({"id"})       //dependent on the variable name. Will need to be updated with variable name changes
 public class User {
 
     private int id;
 
+    @JsonProperty("Full Name")          //Customizes the field name in the response
     @Size(min = 2, message = "Name should have at least 2 characters")
     private String name;
 
+    @JsonIgnore         //removes this field from response. Useful for passwords
     @Past(message = "Birth date should be in the past")
     private LocalDate birthDate;
 
